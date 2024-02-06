517e570ebace92577512ddba69b697144cdf7326
Cyberduc-k <tttymo@gmail.com>
2024-02-06 18:40:57 +0100

    initial commit

---
+++ src/intern.rs
@@ -0,0 +1,115 @@
+use std::collections::HashSet;
+use std::fmt::Debug;
+use std::hash::{Hash, Hasher};
+use std::ops::Deref;
+use std::sync::{OnceLock, PoisonError, RwLock};
+
+/// A trait for internable values.
+pub trait Internable: Hash + Eq {
+    /// Creates a static reference to `self`, possibly leaking memory.
+    fn leak(&self) -> &'static Self;
+
+    /// Returns `true` if the two references point to the same value.
+    fn ref_eq(&self, other: &Self) -> bool;
+
+    /// Feeds the reference to the hasher.
+    fn ref_hash<H: Hasher>(&self, state: &mut H);
+}
+
+impl Internable for str {
+    fn leak(&self) -> &'static Self {
+        let str = self.to_owned().into_boxed_str();
+        Box::leak(str)
+    }
+
+    fn ref_eq(&self, other: &Self) -> bool {
+        self.as_ptr() == other.as_ptr() && self.len() == other.len()
+    }
+
+    fn ref_hash<H: Hasher>(&self, state: &mut H) {
+        self.len().hash(state);
+        self.as_ptr().hash(state);
+    }
+}
+
+/// An interned value. Will stay valid until the end of the program and will not drop.
+pub struct Interned<T: ?Sized + 'static>(pub &'static T);
+
+/// A thread-safe interner which can be used to create [`Interned<T>`] from a `&T`.
+pub struct Interner<T: ?Sized + 'static>(OnceLock<RwLock<HashSet<&'static T>>>);
+
+impl<T: ?Sized> Default for Interner<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: ?Sized> Interner<T> {
+    pub const fn new() -> Self {
+        Self(OnceLock::new())
+    }
+}
+
+impl<T: Internable + ?Sized> Interner<T> {
+    pub fn intern(&self, value: &T) -> Interned<T> {
+        let lock = self.0.get_or_init(Default::default);
+        {
+            let set = lock.read().unwrap_or_else(PoisonError::into_inner);
+            if let Some(value) = set.get(value) {
+                return Interned(*value);
+            }
+        }
+        {
+            let mut set = lock.write().unwrap_or_else(PoisonError::into_inner);
+            if let Some(value) = set.get(value) {
+                Interned(*value)
+            } else {
+                let leaked = value.leak();
+                set.insert(leaked);
+                Interned(leaked)
+            }
+        }
+    }
+}
+
+impl<T: ?Sized> Deref for Interned<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.0
+    }
+}
+
+impl<T: ?Sized> Clone for Interned<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T: ?Sized> Copy for Interned<T> {}
+
+impl<T: ?Sized + Internable> PartialEq for Interned<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.ref_eq(other.0)
+    }
+}
+
+impl<T: ?Sized + Internable> Eq for Interned<T> {}
+
+impl<T: ?Sized + Internable> Hash for Interned<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.ref_hash(state);
+    }
+}
+
+impl<T: ?Sized + Debug> Debug for Interned<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<T> From<&Interned<T>> for Interned<T> {
+    fn from(value: &Interned<T>) -> Self {
+        *value
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+pub mod intern;
+
+use std::any::Any;
+use std::hash::{Hash, Hasher};
+
+pub use paste as __paste;
+
+/// An object safe version of [`Eq`].
+pub trait DynEq: Any {
+    /// Cast the type to `dyn Any`.
+    fn as_any(&self) -> &dyn Any;
+
+    /// This method tests for `self` and `other` values to be equal.
+    fn dyn_eq(&self, other: &dyn DynEq) -> bool;
+}
+
+/// An object safe version of [`Hash`].
+pub trait DynHash: DynEq {
+    /// Cast the type to `dyn DynEq`.
+    fn as_dyn_eq(&self) -> &dyn DynEq;
+
+    /// Feeds this value into the given [`Hasher`].
+    fn dyn_hash(&self, state: &mut dyn Hasher);
+}
+
+impl<T: Any + Eq> DynEq for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn dyn_eq(&self, other: &dyn DynEq) -> bool {
+        if let Some(other) = other.as_any().downcast_ref::<T>() {
+            return self == other;
+        }
+        false
+    }
+}
+
+impl<T: DynEq + Hash> DynHash for T {
+    fn as_dyn_eq(&self) -> &dyn DynEq {
+        self
+    }
+
+    fn dyn_hash(&self, mut state: &mut dyn Hasher) {
+        self.type_id().hash(&mut state);
+        T::hash(self, &mut state);
+    }
+}
+
+#[macro_export]
+macro_rules! define_label {
+    ($(#[$attr:meta])* $label_name:ident) => {
+        pub trait $label_name: ::std::fmt::Debug + Send + Sync + 'static {
+            /// Clones this `
+            #[doc = stringify!($label_name)]
+            /// `.
+            fn dyn_clone(&self) -> ::std::boxed::Box<dyn $label_name>;
+
+            /// Cast the type to `dyn DynEq`.
+            fn as_dyn_eq(&self) -> &dyn $crate::DynEq;
+
+            /// Cast the type to `dyn DynHash`.
+            fn as_dyn_hash(&self) -> &dyn $crate::DynHash;
+
+            /// Returns an [`Interned`](labels::intern::Interned) value corresponding to `self`.
+            fn intern(&self) -> $crate::intern::Interned<dyn $label_name>
+            where
+                Self: Sized,
+            {
+                $crate::__paste::paste! {
+                    [<$label_name:upper _INTERNER>].intern(self)
+                }
+            }
+        }
+
+        impl $label_name for $crate::intern::Interned<dyn $label_name> {
+            fn dyn_clone(&self) -> ::std::boxed::Box<dyn $label_name> {
+                (**self).dyn_clone()
+            }
+
+            fn as_dyn_eq(&self) -> &dyn $crate::DynEq {
+                (**self).as_dyn_eq()
+            }
+
+            fn as_dyn_hash(&self) -> &dyn $crate::DynHash {
+                (**self).as_dyn_hash()
+            }
+
+            fn intern(&self) -> Self {
+                *self
+            }
+        }
+
+        impl PartialEq for dyn $label_name {
+            fn eq(&self, other: &Self) -> bool {
+                self.as_dyn_eq().dyn_eq(other.as_dyn_eq())
+            }
+        }
+
+        impl Eq for dyn $label_name {}
+
+        impl ::std::hash::Hash for dyn $label_name {
+            fn hash<H: ::std::hash::Hasher>(&self, state: &mut H) {
+                self.as_dyn_hash().dyn_hash(state);
+            }
+        }
+
+        impl $crate::intern::Internable for dyn $label_name {
+            fn leak(&self) -> &'static Self {
+                Box::leak(self.dyn_clone())
+            }
+
+            fn ref_eq(&self, other: &Self) -> bool {
+                if self.as_dyn_eq().type_id() != other.as_dyn_eq().type_id() {
+                    return false;
+                }
+                (self as *const Self as *const ()) == (other as *const Self as *const ())
+            }
+
+            fn ref_hash<H: ::std::hash::Hasher>(&self, state: &mut H) {
+                use ::std::hash::Hash;
+                self.as_dyn_eq().type_id().hash(state);
+                (self as *const Self as *const ()).hash(state);
+            }
+        }
+
+        $crate::__paste::paste! {
+            static [<$label_name:upper _INTERNER>]: $crate::intern::Interner<dyn $label_name> =
+                $crate::intern::Interner::new();
+        }
+    };
+}
+
+define_label!(Test);
